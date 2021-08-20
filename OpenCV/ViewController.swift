acaf40ff74dec43783b909a28524d34a8f92478e
seoyeon <hongssup0202@gmail.com>
2021-08-20 14:38:04 +0900

    Initial Commit

---
+++ OpenCV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenCV
+//
+//  Created by SeoYeon Hong on 2021/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
